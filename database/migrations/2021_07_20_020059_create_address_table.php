b7ad3effcb8d01c5f2c5fd15730769ae7d07e3fc
Marlo <marlosilva.f2@gmail.com>
2021-07-27 10:23:47 -0300

    initial

---
+++ database/migrations/2021_07_20_020059_create_address_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->id('id');
+            $table->unsignedBigInteger('id_user');
+            $table->string('cep');
+            $table->string('rua');
+            $table->string('bairro');
+            $table->string('complemento');
+            $table->string('numero');
+            $table->timestamps();
+
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\AddressController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Middleware\JwtMiddleware;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+
+
+Route::group(['namespace' => 'Api', 'as' => 'api.'], function () {
+        Route::post('/auth/login',  [AuthController::class, 'login'])->name('auth.login');
+
+Route::group(['middleware' => 'jwt'], function () {
+
+    Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/refresh',  [AuthController::class, 'refresh'])->name('auth.refresh');
+        Route::post('/auth/logout',  [AuthController::class, 'logout'])->name('auth.logout');
+        Route::post('/me',  [AuthController::class, 'me'])->name('auth.me');
+        Route::get('/users', [UserController::class, 'index'])->name('auth.index');
+        Route::get('/address', [AddressController::class, 'index'])->name('address.index');
+        Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+    });
+});
